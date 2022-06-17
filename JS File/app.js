fff05433d14425ab60183d75caba76a370f0420f
Alisher <alishertogoyev682@gmail.com>
2022-06-17 22:59:53 +0500

    My-project

--- JS File/app.js
+++ JS File/app.js
@@ -152,27 +152,33 @@ navItem.forEach((navItem => {
 }));
 
 
+const link = document.querySelector(".link");
+
+link.addEventListener("click", () => {
+    
+});
+
 
 // Scroll reveal animations
 
 // Common reveal options to creat reveal animations
 
-ScrollReveal({
-    // reset: true,
-    distance: '60px',
-    duration: 2500,
-    delay: 100
-});
+// ScrollReveal({
+//     reset: true,
+//     distance: '60px',
+//     duration: 2500,
+//     delay: 100
+// });
 
 // Target elements, and specify options to create reveal animations
 
-ScrollReveal().reveal('.home .info h2, .section-title-1, .section-title-2 ', { delay: 500, origin: 'left'});
-ScrollReveal().reveal('.home .info h3, .home .info p, about-info .btn', { delay: 600, origin: 'right'});
-ScrollReveal().reveal('.home .info .btn', { delay: 700, origin: 'bottom'});
-ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 500, origin: 'left', interval: 200});
-ScrollReveal().reveal('.home-img, .about-img', { delay: 500, origin: 'bottom'});
-ScrollReveal().reveal('.about .description, .contact-right', { delay: 600, origin: 'right'});
-ScrollReveal().reveal('.about .professional-list li', { delay: 600, origin: 'right', interval: 200});
-ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2', { delay: 700, origin: 'left'});
-ScrollReveal().reveal('.experiance-card, .service-card, education, .portfolio .img-card', { delay: 800, origin: 'bottom', interval: 200});
-ScrollReveal().reveal('footer .group', { delay: 500, origin: 'top', interval: 200});
+// ScrollReveal().reveal('.home .info h2, .section-title-1, .section-title-2 ', { delay: 300, origin: 'left'});
+// ScrollReveal().reveal('.home .info h3, .home .info p, about-info .btn', { delay: 400, origin: 'right'});
+// ScrollReveal().reveal('.home .info .btn', { delay: 500, origin: 'bottom'});
+// ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 300, origin: 'left', interval: 150});
+// ScrollReveal().reveal('.home-img, .about-img', { delay: 300, origin: 'bottom'});
+// ScrollReveal().reveal('.about .description, .contact-right', { delay: 400, origin: 'right'});
+// ScrollReveal().reveal('.about .professional-list li', { delay: 400, origin: 'right', interval: 150});
+// ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2', { delay: 500, origin: 'left'});
+// ScrollReveal().reveal('.experiance-card, .service-card, education, .portfolio .img-card', { delay: 800, origin: 'bottom', interval: 150});
+// ScrollReveal().reveal('footer .group', { delay: 300, origin: 'top', interval: 150});
